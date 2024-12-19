e4bd630d3a7d1f41c21defcefe0760f298f83f34
Lizok28 <elizavetag924@gmail.com>
2024-12-19 13:42:20 +0300

    loli

--- PZ2/Pz_7.py
+++ PZ2/Pz_7.py
@@ -1,9 +1,9 @@
 import math
 
 
-
 def f(x):
-    return math.sin(x)
+   return math.sin(x)
+
 
 a = float(input('Введите a: '))
 b = float(input('Введите b: '))  #Вводим данные
@@ -13,33 +13,23 @@ h = (b-a)/n #Расчитываем h
 
 lol = 0
 
-for i in range(n):
-    lol += f(a+h*(i-1/2))
-print('Методом центральных пямоугольников =', h*lol)
-
-lol = 0
 
 for i in range(n):
-    lol += f(a+h*i)
-print('Методом левых пямоугольников =', h*lol)
+   lol += f(a+h*i)
+print('Методом левых прямоугольников =', h*lol)
 
 
 lol = 0
 
-for i in range(1, n+1):
-    lol += f(a+h*i)
-print('Методом правых пямоугольников =', h*lol)
-
-
-lol = 0
 
 for i in range(n):
-    lol += f((a+i*h)+(h/2))
-print('Методом средних пямоугольников =', h*lol)
+   lol += f(a+h*(i+1))
+print('Методом правых прямоугольников =', h*lol)
 
 
 lol = 0
+
 
 for i in range(1, n):
-    lol += f(a+i*h)
+   lol += f(a+i*h)
 print('Методом трапеции =', h*(f(a)/2 + f(b)/2 + lol))
